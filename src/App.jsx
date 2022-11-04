ea833f512f105574bb401ec59c7f0d466d6aa9d5
vesnushkina19 <kate.vesnushkina@gmail.com>
2022-11-04 23:56:01 +0300

    Добавление шапки

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from "./components/Card";
-
+import Header from "./components/Header/header";
 import data from "./assets/data.json";
 
-class App extends React.Component {
-    render() {
-        return (
-        <div className="wrapper">
-            <div className="cards-container">
-                {/* <Card/> */}
-                {data.map((d, i) => <Card 
-                key={i}
-                img={d.picture}
-                text={d.name}
-                price={d.price}
-                />)}
-            </div>
+const App = () => {
+    const [goods, setGoods] = useState(data);
+
+    return (
+    <div className="wrapper">
+        <Header products={goods} />
+        <div className="cards-container">
+            {/* <Card/> */}
+            {goods.map((d, i) => <Card 
+            key={i}
+            img={d.picture}
+            text={d.name}
+            price={d.price}
+            />)}
         </div>
-        )
-    }
+    </div>
+    )
 }
+
 export default App;
---
+++ src/components/Header/header.jsx
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+import Logo from "../Logo/logo";
+import "./header.css";
+import {ReactComponent as FavIcon} from "./images/ic-favorites.svg";
+import {ReactComponent as CartIcon} from "./images/ic-cart.svg";
+import {ReactComponent as ProfileIcon} from "./images/ic-profile.svg";
+
+
+export default ({products}) => {
+    const [text, changeText] = useState("");
+    const [cnt, setCnt] = useState(0);
+    const handler = e => {
+        changeText(e.target.value);
+        const result = products.filter((el => el.name.toLowerCase().search(e.target.value.toLowerCase()) !== -1));
+        setCnt(result.length);
+    }
+    return <>
+    <header>
+        <Logo/>
+        <input type="search" value={text} onChange={handler}/>
+        <nav>
+            <a href=""><FavIcon/></a>
+            <a href=""><CartIcon/></a>
+            <a href=""><ProfileIcon/></a>
+        </nav>
+    </header>
+    <div>
+        {text ? `По запросу ${text} найдено ${cnt} позиций` : "Поиск..."}
+    </div>
+    </>
+}
+
