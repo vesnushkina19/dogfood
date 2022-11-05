d75c5ae8651bef7325fa1ce761be08d5591aa9ed
unknown <kate.vesnushkina@gmail.com>
2022-11-05 16:26:23 +0300

    add header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -6,13 +6,18 @@ import {ReactComponent as CartIcon} from "./images/ic-cart.svg";
 import {ReactComponent as ProfileIcon} from "./images/ic-profile.svg";
 
 
-export default ({products}) => {
+export default ({products, update}) => {
     const [text, changeText] = useState("");
     const [cnt, setCnt] = useState(0);
     const handler = e => {
         changeText(e.target.value);
         const result = products.filter((el => el.name.toLowerCase().search(e.target.value.toLowerCase()) !== -1));
         setCnt(result.length);
+        if (!text) {
+            update(products);
+        } else {
+            update(result);
+        }
     }
     return <>
     <header>
