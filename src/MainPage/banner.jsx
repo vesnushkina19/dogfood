85fbeb4ccceb904e3c48d809d3f55a4895bd9012
unknown <kate.vesnushkina@gmail.com>
2022-11-11 17:08:52 +0300

    upd main page

--- src/MainPage/banner.jsx
+++ src/MainPage/banner.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Col, Figure } from "react-bootstrap";
+import "./banner.css";
 
 
 export default () => {
@@ -7,16 +8,16 @@ export default () => {
         backgroundColor: "orange",
         borderRadius: "20px",
         height: "250px",
-        zIndex: "333",
-        display: "flex"
+        display: "flex",
+        marginBottom: "20px"
     };
     return <Col xs={12} md={12} style={box}>
     <Col xs={8} md={6}>
         <h2 style={{color:"white", fontWeight:"900", padding:"10px 50px", fontSize:"44px"}}>Подарок за первый заказ!</h2>
         <p style={{color:"white", padding:"10px 50px", fontSize:"20px"}}>Лакомства из говядины</p>
     </Col>
-    <Col xs={4} md={6}>
-        <Figure style={{height:"250px", width: "250px", rotate:"30deg"}}>
+    <Col xs={4} md={6} style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+        <Figure className="rotate">
             <Figure.Image src="https://static.insales-cdn.com/images/products/1/5406/409023774/large_25.png">
             </Figure.Image>
         </Figure>
--- src/MainPage/main.jsx
+++ src/MainPage/main.jsx
@@ -12,9 +12,10 @@ export default () => {
     let p = data[0];
     let p1 = data[1];
     let p2 = data[2];
+
     return <div className="wrapper">
     <Header/>
-        <Container style={{height: "1000px", paddingTop: "20px"}}>
+        <Container style={{paddingTop: "20px"}}>
             <Row>
                 <Banner/>
                 <Col xs={10} md={10} style={{paddingTop:"10px"}}>
@@ -28,28 +29,51 @@ export default () => {
                     </ButtonGroup>
                 </Col>
                 <Col xs={6} md={4} style={{padding:"20px"}}>
-                <Figure style={{borderRadius: "20px"}}>
-                    <Figure.Image src={p.picture}/>
-                </Figure>
-                <div style={{fontWeight: "900"}}>{p.price} ₽</div>    
-                <h1 style={{fontSize: "18px"}}>{p.name}</h1>
-                <Button size="sm" variant="warning">В корзину</Button>
+                    <Figure style={{borderRadius: "20px"}}>
+                        <Figure.Image src={p.picture}/>
+                    </Figure>
+                    <div style={{fontWeight: "900"}}>{p.price} ₽</div>    
+                    <h1 style={{fontSize: "18px"}}>{p.name}</h1>
+                    <Button size="sm" variant="warning">В корзину</Button>
                 </Col>
                 <Col xs={6} md={4} style={{padding:"20px"}}>
-                <Figure>
-                    <Figure.Image src={p1.picture}/>
-                </Figure>
-                <div style={{fontWeight: "900"}}>{p1.price} ₽</div>   
-                <h1 style={{fontSize: "18px"}}>{p1.name}</h1>
-                <Button size="sm" variant="warning">В корзину</Button>
+                    <Figure>
+                        <Figure.Image src={p1.picture}/>
+                    </Figure>
+                    <div style={{fontWeight: "900"}}>{p1.price} ₽</div>   
+                    <h1 style={{fontSize: "18px"}}>{p1.name}</h1>
+                    <Button size="sm" variant="warning">В корзину</Button>
                 </Col>
-                <Col xs={0} md={4} style={{padding:"20px"}}>
-                <Figure>
-                    <Figure.Image src={p2.picture}/>
-                </Figure>
-                <div style={{fontWeight: "900"}}>{p2.price} ₽</div>     
-                <h1 style={{fontSize: "18px"}}>{p2.name}</h1>
-                <Button size="sm" variant="warning">В корзину</Button>
+                <Col xs={6} md={4} style={{padding:"20px"}}>
+                    <Figure>
+                        <Figure.Image src={p2.picture}/>
+                    </Figure>
+                    <div style={{fontWeight: "900"}}>{p2.price} ₽</div>     
+                    <h1 style={{fontSize: "18px"}}>{p2.name}</h1>
+                    <Button size="sm" variant="warning">В корзину</Button>
+                </Col>
+                <Col xs={12} md={5} style={{backgroundColor:  "lightGreen", height: "250px",borderRadius: "20px", marginBottom: "20px", display: "flex", alignItems: "center", justifyContent: "center"}}>
+                    <Col xs={6}>
+                        <h1 style={{color:"white", fontWeight:"900", padding:"10px 20px", fontSize:"34px"}}>Рога северного оленя</h1>
+                        <p style={{color:"white", padding:"10px 20px", fontWeight:"700", fontSize:"15px"}}>От 10 до 30 кг.</p>
+                    </Col>
+                    <Col xs={6}>
+                        <Figure>
+                            <Figure.Image src="https://react-learning.ru/image-compressed/16.jpg" style={{height: "200px"}}/>
+                        </Figure> 
+                    </Col>
+                </Col>
+                <Col></Col>
+                <Col xs={12} md={6} style={{backgroundColor:  "#E0CC49", height: "250px",borderRadius: "20px", display: "flex", alignItems: "center", justifyContent: "center"}}>
+                    <Col>
+                        <h1 style={{color:"white", fontWeight:"900", padding:"10px 20px", fontSize:"34px"}}>Свиные уши</h1>
+                        <p style={{color:"white", padding:"10px 20px", fontWeight:"700", fontSize:"15px"}}>100% натуральные</p>
+                    </Col>
+                    <Col>
+                        <Figure>
+                            <Figure.Image src="https://react-learning.ru/image-compressed/10.jpg" style={{height: "200px"}}/>
+                        </Figure> 
+                    </Col>
                 </Col>
                 <Banner/>
             </Row>
