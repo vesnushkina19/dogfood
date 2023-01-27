1111955072b264f2b10c76f8083b25271e720759
unknown <kate.vesnushkina@gmail.com>
2023-01-27 19:00:56 +0300

    upd

--- src/components/ResetPassword/ResetPassword.jsx
+++
@@ -1,5 +0,0 @@
-export const ResetPassword = () => {
-    return <Form>
-
-    </Form>
-}
--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, "docs"),
 		filename: "main.js",
-		publicPath: "/",
-		publicPath: "/dogfood/"
+		publicPath: "/"
 
 	},
 	devServer: {
